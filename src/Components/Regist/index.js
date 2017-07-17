91ca584280d20218fd4b19df60699df977316572
babyvox917 <295820840@qq.com>
2017-07-17 17:33:51 +0800

    ddd

--- src/Components/Regist/index.js
+++ src/Components/Regist/index.js
@@ -10,7 +10,18 @@ class Regist extends Component{
 	render(){
 		return (
 			<div id="regist">
+				<ul>
+				    <li>输入手机号></li>
+					<li>输入验证码></li>
+					<li>设置密码></li>
+				</ul>
+				<div id="tel">
+				  <input type="text" placeholder="请输入手机号"/>
+				</div>
 				
+				<input type="chexbox" id="ck"/> <p id="pp">我已阅读美团网协议</p>
+				<br/>
+				<button id="btn">获取验证码</button>
 			</div>
 		)
 	}
