794afefddf6699a5ed346faac91583e135550d09
caler <1320591507@qq.com>
2017-07-18 21:26:01 +0800

    lisa

--- src/Components/LoginEmail/index.js
+++ src/Components/LoginEmail/index.js
@@ -7,15 +7,33 @@ import {
 } from "react-router-dom";
 
 
-
 class LoginEmail extends Component{
 	constructor(props) {
 		super(props);
-		
+		this.login = this.login.bind(this);
+		this.state = {
+			regYanZheng:false
+		}
 	}
 
-	componentDidMount(){
-
+	login(){
+			fetch('/api/user/login', {
+				headers: {
+				    'Content-Type': 'application/x-www-form-urlencoded'
+				  },
+				credentials: 'include',
+			  	method: 'POST',
+			  	body: "username=" + this.refs.username.value+"&psw="+ this.refs.psw.value
+			}).then(function(res){
+				return res.text();
+			}).then(function(data){
+				console.log(data);
+				if(data ==="登陆成功"){
+					// browserHistory.push('/mai');
+				}
+				
+				
+			})
 	}
 
 	render() {
@@ -23,18 +41,18 @@ class LoginEmail extends Component{
 			<div id="loginemail">
 				<div className="backgroundWh">
 					<div className="userMsg">
-						<div className="msgIn email">
-							<input className="username" type="text" placeholder="账号名/手机号/Email"/>					
+						<div className="msgIn email" onBlur={this.handleBlur.bind(this)}>
+							<input className="username" type="text" placeholder="账号名/手机号/Email" ref ="username"/>					
+							<span></span>
 						</div>
-						<div className="msgIn mima">
-							<input className="paw" type="password" placeholder="请输入您的密码"/>					
+						<div className="msgIn mima" onBlur={this.handleBlur.bind(this)}>
+							<input className="password" type="password" placeholder="请输入您的密码" ref = "psw"/>					
+							<span></span>
 						</div>
 					</div>
 				</div>
 				<div className="submit">
-					<button onClick={()=>{
-						this.props.history.push('/longinafter')
-					}}>登录</button>
+					<button onClick={this.handleClickbtn.bind(this)}>登录</button>
 					<div>
 						<NavLink to="/regist" activeClassName="active" className="regNow left">立即注册</NavLink>
 						<NavLink to="/help" activeClassName="active" className="regNow right" >找回密码</NavLink>
@@ -45,6 +63,49 @@ class LoginEmail extends Component{
 		)
 	}
 
+		//表单验证
+		handleBlur(){
+			var username = document.querySelector('.username').value;
+			var password = document.querySelector('.password').value;
+			var regUser = /^[a-zA-z][a-zA-Z0-9_]{2,9}$/;
+			var regPass = /\d[6-8]/;
+
+			var spanUser = document.querySelector('.email span');
+			var spanPass = document.querySelector('.mima span');
+			var email = document.querySelector('.email');
+			var mima = document.querySelector('.mima');
+
+
+			if(regUser.test(username)){
+				
+				spanUser.innerHTML = "正确";
+				mima.style.borderBottom = 0; 
+			}else{
+				spanUser.innerHTML = "格式不对";
+				email.style.borderBottom = '1px solid red'; 
+			}
+
+			if(regPass.test(password)){
+				
+				spanPass.innerHTML = "正确";	
+				mima.style.borderBottom = 0; 
+			}else{
+				spanPass.innerHTML = "格式不对";
+				mima.style.borderBottom = '1px solid red'; 
+			}
+
+			if (regUser.test(username) && regPass.test(password)) {
+				this.setState({
+					regYanZheng:true
+				})
+			}
+		}
+
+
+		//后台验证发ajax
+		handleClickbtn(){
+
+		}
 
 }
 
