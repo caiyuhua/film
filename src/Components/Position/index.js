46555133f317925262eedb66671278d77f88837b
unknown <>
2017-07-18 19:50:02 +0800

    wp

--- src/Components/Position/index.js
+++ src/Components/Position/index.js
@@ -10,7 +10,8 @@ class Position extends Component{
 	constructor(props){
 		super(props);
 		this.state={
-			isShow:"hide"
+			isShow:"hide",
+			currentIndex:"0"
 		}
 	}
 
@@ -18,17 +19,38 @@ class Position extends Component{
 		return (
 			<div id="position">
 				<ul id="_city">
-					<li onClick={this.aClick.bind(this)}>热门城市<i className="iconfont">&#xe6a6;</i></li>
-					<ul className={this.state.isShow?"hide":"show"}>
-						<li>北京</li>
-						<li>上海</li>
-						<li>深圳</li>
-						<li>天津</li>
-						<li>杭州</li>
-					</ul>
-					<li>A<i className="iconfont">&#xe6a6;</i></li>
-					<li>B<i className="iconfont">&#xe6a6;</i></li>
-					<li>C<i className="iconfont">&#xe6a6;</i></li>
+					<li data-index="0" onClick={this.aClick.bind(this)}>热门城市<i className="iconfont">&#xe6a6;</i></li>
+						<ul className={this.state.currentIndex=="0"?"show":"hide"}>
+						<li><NavLink to="/App" activeClassName="selected">北京</NavLink></li>
+						<li><NavLink to="/App" activeClassName="selected">上海</NavLink></li>
+						<li><NavLink to="/App" activeClassName="selected">深圳</NavLink></li>
+						<li><NavLink to="/App" activeClassName="selected">天津</NavLink></li>
+						<li><NavLink to="/App" activeClassName="selected">杭州</NavLink></li>
+						</ul>
+					<li data-index="1" onClick={this.aClick.bind(this)}>A<i className="iconfont">&#xe6a6;</i></li>
+						<ul className={this.state.currentIndex=="1"?"show":"hide"}>
+						<li><NavLink to="/App" activeClassName="selected">安阳</NavLink></li>
+						<li><NavLink to="/App" activeClassName="selected">安溪</NavLink></li>
+						<li><NavLink to="/App" activeClassName="selected">澳门</NavLink></li>
+						<li><NavLink to="/App" activeClassName="selected">安平</NavLink></li>
+						<li><NavLink to="/App" activeClassName="selected">安顺</NavLink></li>
+						</ul>
+					<li data-index="2" onClick={this.aClick.bind(this)}>B<i className="iconfont">&#xe6a6;</i></li>
+						<ul className={this.state.currentIndex=="2"?"show":"hide"}>
+						<li><NavLink to="/App" activeClassName="selected">北京</NavLink></li>
+						<li><NavLink to="/App" activeClassName="selected">保定</NavLink></li>
+						<li><NavLink to="/App" activeClassName="selected">包头</NavLink></li>
+						<li><NavLink to="/App" activeClassName="selected">本溪</NavLink></li>
+						<li><NavLink to="/App" activeClassName="selected">北海</NavLink></li>
+						</ul>
+					<li data-index="3" onClick={this.aClick.bind(this)}>C<i className="iconfont">&#xe6a6;</i></li>
+						<ul className={this.state.currentIndex=="3"?"show":"hide"}>
+						<li><NavLink to="/App" activeClassName="selected">重庆</NavLink></li>
+						<li><NavLink to="/App" activeClassName="selected">长沙</NavLink></li>
+						<li><NavLink to="/App" activeClassName="selected">长春</NavLink></li>
+						<li><NavLink to="/App" activeClassName="selected">沧州</NavLink></li>
+						<li><NavLink to="/App" activeClassName="selected">从化</NavLink></li>
+						</ul>
 					<li>D<i className="iconfont">&#xe6a6;</i></li>
 					<li>E<i className="iconfont">&#xe6a6;</i></li>
 					<li>F<i className="iconfont">&#xe6a6;</i></li>
@@ -53,13 +75,11 @@ class Position extends Component{
 
 		)
 	}
-	aClick(){
+	aClick(ev){
 		this.setState({
-			isShow:!this.state.isShow
+			isShow:!this.state.isShow,
+			currentIndex:ev.target.dataset.index
 		})
-	}
-	bClick(){
-		w.location.href='你的地址'
 	}
 }
 export default Position;
