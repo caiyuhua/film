37ed763705bf61882de41a337f7928729c83f26f
caler <1320591507@qq.com>
2017-07-19 15:59:50 +0800

    lisa

--- src/Components/LoginEmail/index.js
+++ src/Components/LoginEmail/index.js
@@ -31,8 +31,7 @@ class LoginEmail extends Component{
 				if(data ==="登陆成功"){
 					// browserHistory.push('/mai');
 				}
-				
-				
+								
 			})
 	}
 
@@ -46,7 +45,7 @@ class LoginEmail extends Component{
 							<span></span>
 						</div>
 						<div className="msgIn mima" onBlur={this.handleBlurb.bind(this)}>
-							<input className="password" type="password" placeholder="请输入您的密码" ref = "psw"/>					
+							<input className="password" type="password" placeholder="请输入您的密码" ref = "password"/>					
 							<span></span>
 						</div>
 					</div>
@@ -66,7 +65,7 @@ class LoginEmail extends Component{
 		//表单验证
 		handleBlura(){
 			var username = document.querySelector('.username').value;
-			var regUser = /^[a-zA-z][a-zA-Z0-9_]{2,9}$/;		
+			var regUser = /^[a-zA-z0-9][a-zA-Z0-9_]{2,13}$/;		
 			var spanUser = document.querySelector('.email span');			
 			var email = document.querySelector('.email');
 			var mima = document.querySelector('.mima');
@@ -103,11 +102,24 @@ class LoginEmail extends Component{
 			// 	})
 			// }
 		}
-
+	
 
 		//后台验证发ajax
 		handleClickbtn(){
-
+			fetch('Index/index/login4ajax',{
+				headers: {
+				    'Content-Type': 'application/x-www-form-urlencoded'
+				  },
+				credentials: 'include',
+			  	method: 'POST',
+			  	body: "username=" + this.refs.username.value+"&psw="+ this.refs.password.value
+			}).then(function(res){
+				return res.text();
+			}).then(function(data){
+				console.log(data);
+				
+				// this.props.history.push('/home/movie');
+			})
 		}
 
 }
